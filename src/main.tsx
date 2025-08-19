998fc6e2adddd86a64be1c0195c4f2603c394aee
ahmadammarm <ahmad.ammar.2205356@students.um.ac.id>
2025-08-19 09:45:10 +0700

    feat(auth): implement user registration form and update routing

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from "react-router"
 import Layout from './components/Layout'
+import UserRegisterForm from './components/auth/UserRegisterForm'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
         <Routes>
             <Route element={<Layout />}>
-                <Route path="/register" element={<div>Register</div>} />
+                <Route path="/register" element={<UserRegisterForm />} />
             </Route>
         </Routes>
     </BrowserRouter>

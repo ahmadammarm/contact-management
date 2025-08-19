dd7f9e9d50282d06c16649a01baf94107b897d15
ahmadammarm <ahmad.ammar.2205356@students.um.ac.id>
2025-08-19 09:39:24 +0700

    feat(layout): add layout component for not authenticated users

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from "react-router"
+import Layout from './components/Layout'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
         <Routes>
-            <Route element={<div>App</div>}>
+            <Route element={<Layout />}>
                 <Route path="/register" element={<div>Register</div>} />
             </Route>
         </Routes>
